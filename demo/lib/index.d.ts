fb1436cdb2a29592042ad002fe17da36f6370c54
michaelficarra <github@michael.ficarra.me>
2025-07-01 20:35:54 +0000

    Deploying to gh-pages from @ tc39/proposal-iterator-chunking@c5fe8844472aefdb3808e4d60fd5d46e50e4b9ac 🚀

--- demo/lib/index.d.ts
+++ demo/lib/index.d.ts
@@ -4,3 +4,5 @@ declare function chunksImpl<A>(iter: Iterator<A>, chunkSize: number): Generator<
 declare function chunks<A>(this: Iterator<A>, chunkSize: number): Generator<Array<A>>;
 declare function windowsImpl<A>(iter: Iterator<A>, windowSize: number): Generator<Array<A>>;
 declare function windows<A>(this: Iterator<A>, windowSize: number): Generator<Array<A>>;
+declare function slidingImpl<A>(iter: Iterator<A>, windowSize: number): Generator<Array<A>>;
+declare function sliding<A>(this: Iterator<A>, windowSize: number): Generator<Array<A>>;
